8623b4d789bcccda5fd662b3f23c2094fdf14614
arosa81 <alex.j.rosa@gmail.com>
2015-08-18 06:41:07 -0600

    correcting code indentation and single quote attributes

--- js/drawer-Menu.js
+++ js/drawer-Menu.js
@@ -1,15 +1,15 @@
-  /*
-   * Open the drawer when the menu ison is clicked.
-   */
-  var menu = document.querySelector('#menu');
-  var main = document.querySelector('main');
-  var drawer = document.querySelector('.nav');
+/*
+* Open the drawer when the menu ison is clicked.
+*/
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('.nav');
 
-  menu.addEventListener('click', function(e) {
-    drawer.classList.toggle('open');
-    e.stopPropagation();
-  });
+menu.addEventListener('click', function(e) {
+  drawer.classList.toggle('open');
+  e.stopPropagation();
+});
 
-  main.addEventListener('click', function() {
-    drawer.classList.remove('open');
-  });
+main.addEventListener('click', function() {
+  drawer.classList.remove('open');
+});
